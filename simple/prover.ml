f1fe3f579fff656c199ca7a595e92cb3244fa5af
garou404 <charlesmeyer347@gmail.com>
2024-11-26 16:12:23 +0100

    type inference and type checking implemented

--- simple/prover.ml
+++ simple/prover.ml
@@ -41,3 +41,48 @@ let rec string_of_tm tm =
 let test = Abs(Imp(Type "A", Type "B"), "f", Abs(Type "A", "x", App(Var "f", Var "x")))
 
 let () = print_endline(string_of_tm(test))
+
+type context = (var * ty) list
+
+exception Type_error
+
+let rec infer_type ctxt tm =
+  match tm with
+  | Var x -> List.assoc x ctxt
+  | App(a, b) -> (
+    let type_a = infer_type ctxt a in
+    let type_b = infer_type ctxt b in
+    match type_a with
+    | Imp(x, y) ->(
+      match x = type_b with
+      | true -> y
+      | false -> raise Type_error
+    )
+    | _ -> raise Type_error
+  )
+  | Abs(ty, v, t) -> Imp(ty, infer_type ctxt t)
+
+
+let test = Abs(Imp(Type "A", Type "B"), "f", Abs(Imp(Type "B", Type "C"), "g", Abs(Type "A", "x", App(Var "g", App(Var "f", Var "x")))))
+
+let u = string_of_ty (infer_type ([("f", Imp(Type "A", Type "B")); ("g", Imp(Type "B", Type "C")); ("x", Type "A")]) test)
+
+
+let check_type ctxt tm ty =
+  if infer_type ctxt tm = ty then () else raise Type_error
+
+let () = check_type ([("x", Type "A")]) (Abs (Type "A", "x", Var "x")) (Imp (Type "A", Type "A"))
+
+let () = check_type ([("x", Type "A")]) (Abs (Type "A", "x", Var "x")) (Imp (Type "B", Type "B"))
+
+let () = check_type ([]) (Var "x") (Type "A")
+
+(*Mutually recursive:*)
+
+let rec even n =
+  if n = 0 then true
+  else odd (n-1)
+
+and odd n =
+  if n = 0 then false
+  else even (n-1)
